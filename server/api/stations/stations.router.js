24c8be12f1c0a3b3b560c3dfe3f912ac3d404688
Ryan Dwyer <ryankdwyer@gmail.com>
2015-10-23 09:31:34 -0400

    cleaned up dependencies

--- server/api/stations/stations.router.js
+++ server/api/stations/stations.router.js
@@ -3,6 +3,8 @@ var router = require('express').Router();
 var Citibike = require('citibike');
 var citibike = new Citibike();
 
+console.log('Citibike module, ', citibike);
+
 router.get('/stations', function(req, res, next){
 	Station.find()
 	.then(function(stations){
